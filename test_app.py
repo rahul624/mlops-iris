d44345efa79ff573fa714db64af61b9f6e18e9b2
rahul <rahulkaza.1307@gmail.com>
2021-07-11 15:57:19 +0530

    Commiting

--- test_app.py
+++ test_app.py
@@ -9,6 +9,15 @@ def test_ping():
         assert response.status_code == 200
         assert response.json() == {"ping": "pong"}
 
+# test to check the correct functioning of the /feedback loop route
+def test_ping():
+    with TestClient(app) as client:
+        response = client.post("/feedback_loop")
+        # asserting the correct response is received
+        print (response)
+    #  assert response.status_code == 200
+        assert response.json() == {"detail": "Feedback loop successful"}
+
 
 # test to check if Iris Virginica is classified correctly
 def test_pred_virginica():
@@ -24,3 +33,18 @@ def test_pred_virginica():
         # asserting the correct response is received
         assert response.status_code == 200
         assert response.json() == {"flower_class": "Iris Virginica"}
+
+# test to check if Iris Setosa is classified correctly
+def test_pred_setosa():
+    # defining a sample payload for the testcase
+    payload = {
+        "sepal_length": 4,
+        "sepal_width": 3,
+        "petal_length": 5.2,
+        "petal_width": 4.4,
+    }
+    with TestClient(app) as client:
+        response = client.post("/predict_flower", json=payload)
+        # asserting the correct response is received
+        assert response.status_code == 200
+        assert response.json() == {"flower_class": "Iris Setosa"}
