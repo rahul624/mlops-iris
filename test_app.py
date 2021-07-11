2edc74219f584c6c5c3fe991b619e2edece88a57
rahul <rahulkaza.1307@gmail.com>
2021-07-11 17:40:20 +0530

    Commit on

--- test_app.py
+++ test_app.py
@@ -5,7 +5,8 @@ from main import app
 def test_ping():
     with TestClient(app) as client:
         response = client.get("/ping")
-        # asserting the correct response is re
+        # asserting the correct response is received
+        assert response.status_code == 200
         assert response.json() == {"ping": "pong"}
 
 
